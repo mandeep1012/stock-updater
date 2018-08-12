cdd384112d9ebe3d2608de6933e370f9979c1319
mandeep1012 <mandeep1154@gmail.com>
2018-08-12 17:31:22 +0530

    working1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,6 +15,14 @@ export class AppComponent {
    check() {
     if (this.stock1 > 0) {
       this.stock1 = this.stock1 - 10;
+}}
+ check1() {
+  if (this.stock2 > 0) {
+    this.stock2 = this.stock2 - 10;
+}}
+ check2() {
+  if (this.stock3 > 0) {
+    this.stock3 = this.stock3 - 10;
 }
 }
 }
